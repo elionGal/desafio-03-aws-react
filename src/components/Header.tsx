d6096011884e488c87b1000ba15ffde0ef8c89d2
elionGal <eliongalvao@gmail.com>
2024-11-07 14:33:12 -0300

    feat: header style avanced

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
+import "../styles/HeaderStyle.css"
+
 function Header() {
 
   return (
     <>
+    <div className="box">
         <span>Início</span>
         <span>Minha história</span>
         <span>Experiências</span>
         <span>Contato</span>
-        <button type="button">Entrar</button>
+    </div>
+    <button type="button" className="enterLeaveButton">Entrar</button>
     </>
   )
 }
