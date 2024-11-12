be5d8464a67aef7e122afa28ccd7730a57723772
elionGal <eliongalvao@gmail.com>
2024-11-12 14:49:20 -0300

    feat: HistoryPort component added

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -5,6 +5,7 @@ import "../styles/PortfolioStyle.css";
 import { FaPen } from "react-icons/fa";
 import { FaCheck } from "react-icons/fa";
 import { useEffect, useState } from "react";
+import HistoryPort from "../components/HistoryPort";
 
 interface Profile {
   avatar_url: string;
@@ -170,22 +171,7 @@ function Portfolio() {
           )}
         </div>
         <section id="minhaHistoria"></section>
-        <div className="myHistory">
-          <h2 className="historyTitle">Minha história</h2>
-          <p className="historyText">
-            Olá, eu sou Felipe Pato e comecei minha carreira trabalhando em um
-            pequeno escritório na California para meu chefe Elon Musk, fui
-            demitido 2 dias depois por commitar errado e apagar o repositório da
-            empresa. Voltei então a trabalhar de casa criando sites de Yoga para
-            idosos, ganhando muito dinheiro no processo, esses sites eram
-            desenvolvidos usando Fortro e Cobol, ao qual aprendi com meu tio
-            Jhon. No entanto, essa forma de fazer sites não durou muito com o
-            advento das IAs, foi então que decidi estudar muito e acabei
-            passando no processo seletivo da cicada 3301, me tornando assim um
-            grande dev da minha area, hoje trabalho espionando hackers e
-            destruindo governos, eu sou Felipe Pato!
-          </p>
-        </div>
+        <HistoryPort isEditing={isEditing} />
         <section id="experiencias"></section>
         <div className="xp">
           <h2>Experiências</h2>
