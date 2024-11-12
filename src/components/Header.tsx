9e67a34ae83a8850ff4ccff0e22404c550b45da9
elionGal <eliongalvao@gmail.com>
2024-11-12 12:31:36 -0300

    fix: bug position fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,24 @@
-import "../styles/HeaderStyle.css"
+import "../styles/HeaderStyle.css";
 
 function Header() {
+  const scrollToSection = (id: string) => {
+    const section = document.getElementById(id) as HTMLElement | null;
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
     <>
-    <div className="box">
-        <span>Início</span>
-        <span>Minha história</span>
-        <span>Experiências</span>
-        <span>Contato</span>
-    </div>
-    <button type="button" className="enterLeaveButton">Entrar</button>
+      <div className="box">
+        <span onClick={() => scrollToSection("inicio")}>Início</span>
+        <span onClick={() => scrollToSection("minhaHistoria")}>Minha história</span>
+        <span onClick={() => scrollToSection("experiencias")}>Experiências</span>
+        <span onClick={() => scrollToSection("contato")}>Contato</span>
+      </div>
+      <button type="button" className="enterLeaveButton">Entrar</button>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
